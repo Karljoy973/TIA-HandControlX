494da96329369eb4024e0985c283d46cac242942
Karl-Maison <karljoyeux@gmail.com>
2023-06-21 23:16:25 +0200

    suppress everything

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Karlj/Cours/Cours s8/traitement d'images/projet/TIA-HandControlX
-# Build directory: C:/Users/Karlj/Cours/Cours s8/traitement d'images/projet/TIA-HandControlX/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- test.c
+++
@@ -1,8 +0,0 @@
-
-
-int main() {
-    int result = calculate(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
-    int result = calculate(a, b, c, d, e, f, g, h);
-
-
-}
